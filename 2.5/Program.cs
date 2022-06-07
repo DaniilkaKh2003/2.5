d1b05f16b56b45c577d502fe2559bf9716a464bb
Даниил <@LAPTOP-EEPRU07S>
2022-06-07 21:39:39 +0700

    Добавьте файлы проекта.

---
+++ 2.5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("количество элементов массива = ");
+            int N = int.Parse(Console.ReadLine());
+            Random R = new Random();
+            int[] A = new int[N];
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = R.Next(-100, 100);
+                Console.WriteLine(A[i]);
+            }
+            Console.WriteLine("\nНовый массив:");
+            int[] mas = new int[N];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int a = R.Next(1, mas.Length + 1);
+                if (!mas.Contains(a)) mas[i] = a;
+                else i--;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(A[mas[i] - 1]);
+            }
+        }
+    }
+}
